7d6cc0c2b01078c550ac00295372ccdaa6252522
Anna Beatriz <anna.b.valentim@aluno.senai.br>
2025-03-06 09:07:11 -0300

    first commit

---
+++ my-app/pages/index.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+    return (
+    <div className={styles.container}>
+        <div className={styles.login}>
+            <div className={styles.email}></div>
+            <div className={styles.password}></div>
+    </div>
+    </div>
+    );
+}
