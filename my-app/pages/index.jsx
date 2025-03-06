9c369c0f6dfee5b92f08d5a5304681bf3f2da49a
Anna Beatriz <anna.b.valentim@aluno.senai.br>
2025-03-06 09:31:39 -0300

    update

--- my-app/pages/index.jsx
+++ my-app/pages/index.jsx
@@ -2,17 +2,20 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
     return (
-    <div className={styles.container}>
-        <div className={styles.login}>
-            <img src="/optionmonster-logo.png" alt="Logo"></img>
-            <h1>Login</h1>
-            <div className={styles.email}></div>
-            <h1>Email Address</h1>
-            <input type="text" id="taskInput" placeholder="Digite seu email"></input>
-            <h1>Password</h1>
-            <div className={styles.password}></div>
-            <input type="text" id="taskInput" placeholder="Digite sua senha"></input>
-    </div>
-    </div>
+        <div className={styles.container}>
+            <div className={styles.login}>
+                <img src="/optionmonster-logo.png" alt="Logo"></img>
+                <h1>Login</h1>
+            </div>
+            <div className={styles.email}>
+                <p>Email Address</p>
+                <input type="text" id="taskInput" placeholder="Digite seu email"></input>
+            </div>
+            <div className={styles.password}>
+                <p>Password</p>
+                <input type="text" id="taskInput" placeholder="Digite sua senha"></input>
+            </div>
+
+        </div>
     );
 }
